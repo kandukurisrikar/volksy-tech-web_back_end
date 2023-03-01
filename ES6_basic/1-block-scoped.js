22f36b6ec7c7592e1b2864525dd53ddbbdee4761
root <ksrikar@brightgroup.com>
2023-03-01 11:40:11 +0000

    1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,10 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-	/* eslint-disable */
+    /* eslint-disable */
     const task = true;
     const task2 = false;
-	/* eslint-disable */
+    /* eslint-enable */
   }
 
   return [task, task2];
